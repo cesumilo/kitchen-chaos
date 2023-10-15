c21da96ca19451c5bbe19899ac1a7f3c750e059e
Guillaume Robin <10772294+cesumilo@users.noreply.github.com>
2023-10-15 15:01:12 +0200

    Update for windows

--- scripts/KitchenCounter.cs
+++ scripts/KitchenCounter.cs
@@ -3,62 +3,62 @@ using System;
 
 public partial class KitchenCounter : InteractableStaticBody3D, Interactable, IKitchenObjectHolder
 {
-    [Export] KitchenObjectResource kitchenObject;
-    Marker3D spawnPosition;
-    KitchenObject objectHolder;
+	[Export] KitchenObjectResource kitchenObject;
+	Marker3D spawnPosition;
+	KitchenObject objectHolder;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        spawnPosition = GetNode<Marker3D>("SpawnPosition");
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		spawnPosition = GetNode<Marker3D>("SpawnPosition");
+	}
 
-    public void Interact(Player player)
-    {
-        if (objectHolder == null && !player.IsHoldingKitchenObject())
-        {
-            KitchenObject obj = kitchenObject.Instantiate<KitchenObject>();
-            SetKitchenObject(obj);
-        }
-        else if (objectHolder == null) {
-            SetKitchenObject(player.GetKitchenObject());
-            player.ClearKitchenObject();
-        }
-        else if (!player.IsHoldingKitchenObject())
-        {
-            player.SetKitchenObject(objectHolder);
-            ClearKitchenObject();
-        }
-    }
+	public void Interact(Player player)
+	{
+		if (objectHolder == null && !player.IsHoldingKitchenObject())
+		{
+			KitchenObject obj = kitchenObject.Instantiate<KitchenObject>();
+			SetKitchenObject(obj);
+		}
+		else if (objectHolder == null) {
+			SetKitchenObject(player.GetKitchenObject());
+			player.ClearKitchenObject();
+		}
+		else if (!player.IsHoldingKitchenObject())
+		{
+			player.SetKitchenObject(objectHolder);
+			ClearKitchenObject();
+		}
+	}
 
-    public Marker3D GetKitchenObjectSpawn()
-    {
-        return spawnPosition;
-    }
+	public Marker3D GetKitchenObjectSpawn()
+	{
+		return spawnPosition;
+	}
 
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        objectHolder = kitchenObject;
-        objectHolder.SetHolder(this);
-        spawnPosition.AddChild(objectHolder);
-    }
+	public void SetKitchenObject(KitchenObject kitchenObject)
+	{
+		objectHolder = kitchenObject;
+		objectHolder.SetHolder(this);
+		spawnPosition.AddChild(objectHolder);
+	}
 
-    public KitchenObject GetKitchenObject()
-    {
-        return objectHolder;
-    }
+	public KitchenObject GetKitchenObject()
+	{
+		return objectHolder;
+	}
 
-    public void ClearKitchenObject()
-    {
-        if (objectHolder != null)
-        {
-            spawnPosition.RemoveChild(objectHolder);
-        }
-        objectHolder = null;
-    }
+	public void ClearKitchenObject()
+	{
+		if (objectHolder != null)
+		{
+			spawnPosition.RemoveChild(objectHolder);
+		}
+		objectHolder = null;
+	}
 
-    public bool IsHoldingKitchenObject()
-    {
-        return objectHolder != null;
-    }
+	public bool IsHoldingKitchenObject()
+	{
+		return objectHolder != null;
+	}
 }
